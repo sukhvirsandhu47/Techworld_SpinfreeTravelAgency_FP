5ffea9f90db4deaa33644654678f15b23e50cf5e
Naina <maheynaina@gmail.com>
2021-03-17 12:13:59 +0530

    Registration indexdb code

---
+++ js/reg.js
@@ -0,0 +1,16 @@
+// Create needed constants
+const usrIdInput = document.querySelector('#email');
+const pwdInput = document.querySelector('#usrpwd');
+const usrNameInput = document.querySelector('#name');
+
+const urlInput = document.querySelector('#usrurl');
+const yearInput = document.querySelector('#usrbirth');
+
+
+const commentInput = document.querySelector('#comments');
+const chkInput = document.querySelector('#chk');
+
+
+
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('form button');
