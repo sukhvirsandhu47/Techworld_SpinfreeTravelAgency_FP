d1e7375b3078628320b0c2d1a3b53920a7a98e9f
Sukhvir <sukhvir2503@yahoo.com>
2021-03-17 15:48:07 +0530

    reg.js modified

--- js/reg.js
+++ js/reg.js
@@ -133,3 +133,35 @@ e.preventDefault();
 
 
 };
+
+
+$("form input").change(function() {
+  var numValid = 0; 
+  $("form input[required]").each(function() {
+      if (this.validity.valid) {
+          numValid++;       
+      }
+  });
+  var progress = $("#progress");
+if (numValid == 0) {
+  progress.attr("value", "0");
+  }
+if (numValid == 1) {
+  progress.attr("value", "25");
+  
+}
+if (numValid == 2) {
+  progress.attr("value", "50");
+  
+}
+if (numValid == 3) {
+  progress.attr("value", "75");
+  
+}
+if (numValid == 4) {
+  progress.attr("value", "100");
+  
+}
+
+});
+
