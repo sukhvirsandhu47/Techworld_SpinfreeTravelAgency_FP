d8830b3a6b1f6da2d37696c69cd2998f404a3f74
Naina <maheynaina@gmail.com>
2021-03-17 15:05:04 +0530

    reg.js modified

--- js/reg.js
+++ js/reg.js
@@ -54,3 +54,82 @@ window.onload = function() {
    objectStore.createIndex('usrcomments', 'usrcomments', { unique: false });
    
     //
+    
+   let objectStore2 = db.createObjectStore('sessions', { keyPath: 'sessionusrid'});
+   objectStore2.createIndex('date', 'date', { unique: false });
+//
+//
+//
+let objectStore3 = db.createObjectStore('flightbooking', { keyPath: 'id', autoIncrement:true });
+objectStore3.createIndex('usrid', 'usrid', { unique: false });   
+objectStore3.createIndex('flightfrom', 'flightfrom', { unique: false });
+objectStore3.createIndex('flightto', 'flightto', { unique: false });
+objectStore3.createIndex('flightdate', 'flightdate', { unique: false });
+objectStore3.createIndex('flightpassengers', 'flightpassengers', { unique: false });
+//
+let objectStore4 = db.createObjectStore('hotelbooking',  { keyPath: 'id', autoIncrement:true });
+objectStore4.createIndex('usrid', 'usrid', { unique: false });   
+objectStore4.createIndex('hotelfrom', 'hotelfrom', { unique: false });
+objectStore4.createIndex('hotelto', 'hotelto', { unique: false });
+objectStore4.createIndex('hotelname', 'hotelname', { unique: false });
+objectStore4.createIndex('cityname', 'cityname', { unique: false });
+//
+    console.log('Database setup complete');
+  //  alert("setup done");
+  };
+
+  // Create an onsubmit handler so that when the form is submitted the addData() function is run
+  form.onsubmit = addData;
+
+  // Define the addData() function
+  function addData(e) {
+// prevent default - we don't want the form to submit in the conventional way
+e.preventDefault();
+    if(chkInput.checked)
+    {
+       
+    
+    const gender = document.querySelector('input[name="gender"]:checked').value;
+
+
+    // grab the values entered into the form fields and store them in an object ready for being inserted into the DB
+    let newItem = { usrid: usrIdInput.value, usrpwd: pwdInput.value,
+                    usrname:  usrNameInput.value, usrurl: urlInput.value, usrbirth: yearInput.value,
+                  usrgen: gender, usrcomments:   commentInput.value };
+
+    // open a read/write db transaction, ready for adding the data
+    let transaction = db.transaction(['users'], 'readwrite');
+
+    // call an object store that's already been added to the database
+    let objectStore = transaction.objectStore('users');
+
+    // Make a request to add our newItem object to the object store
+    var request = objectStore.add(newItem);
+    request.onsuccess = function() {
+      alert("Registration Successful..Redirecting to Login Page !!");
+     
+      window.location.href = "index.html";
+     
+    };
+
+    // Report on the success of the transaction completing, when everything is done
+    transaction.oncomplete = function() {
+      console.log('Transaction completed: database modification finished.');
+      
+    };
+
+    transaction.onerror = function() {
+      console.log('Transaction not opened due to error');
+    };
+    } 
+    else
+    {
+        alert("Kindly confirm the checkbox before proceeding");
+    }
+
+
+  }
+
+
+
+};
