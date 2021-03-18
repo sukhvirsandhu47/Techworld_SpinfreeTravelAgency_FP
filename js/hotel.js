469eee6605821d8e593578c764e0de9a93a6d597
Naina <maheynaina@gmail.com>
2021-03-18 13:11:09 +0530

    hotel.js

---
+++ js/hotel.js
@@ -0,0 +1,11 @@
+// Create needed constants
+const usrIdInput = document.querySelector('#email');
+const hotelfromInput = document.querySelector('#hotelfrom');
+const hoteltoInput = document.querySelector('#hotelto');
+
+const hotelnameInput = document.querySelector('#hotelname');
+const citynameInput = document.querySelector('#cityname');
+
+
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('form button');
